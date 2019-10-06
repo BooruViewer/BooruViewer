87bbbf05d11a047d425fdd194aa185e070205af0
Nikey646 <nikey646@hotmail.com>
2019-10-06 19:14:55 +1100

    [Client] Added VueX Store Module for the Booru API

---
+++ src/Client/store/api.js
@@ -0,0 +1,49 @@
+import { BooruApi, Endpoints } from "../assets/booru-api"
+
+export const api = {
+  getters: {
+    Instance: "instance",
+    CurrentEndpoint: "currentEndpoint",
+   AllEndpoints: "allEndpoints",
+  },
+  mutations: {
+    ChangeEndpoint: "changeEndpoint",
+  },
+  actions: {
+    Initialize: "init",
+  },
+}
+
+export const state = () => ({
+  booru: null,
+  currentEndpoint: "Safe Danbooru",
+})
+
+export const getters = {
+  [api.getters.Instance]: s => s.booru,
+  [api.getters.CurrentEndpoint]: s => s.currentEndpoint,
+  [api.getters.AllEndpoints]: s => Object.keys(Endpoints)
+}
+
+export const mutations = {
+  [api.mutations.ChangeEndpoint](state, data) {
+    const endpoint = data.endpoint || data
+    if (!Endpoints[endpoint])
+      throw new Error(`Unknown Endpoint "${endpoint}"`)
+
+    state.booru = new BooruApi(Endpoints[endpoint])
+    state.currentEndpoint = endpoint
+  },
+}
+
+export const actions = {
+  [api.actions.Initialize]({ state, commit }) {
+
+    if (state.booru != null) {
+      return
+    }
+
+    console.log(`[VueX/api] Initializing Booru Api with endpoint ${state.currentEndpoint}`)
+    commit('changeEndpoint', { endpoint: state.currentEndpoint });
+  }
+}
