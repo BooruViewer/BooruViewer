2950e6a2f5f1da217477535acdf7b41643b1b444
Nikey646 <nikey646@hotmail.com>
2019-10-06 14:36:48 +1100

    Added the Booru API class for access the server's Booru API

---
+++ src/Client/assets/booru-api.js
@@ -0,0 +1,74 @@
+import qs from "qs"
+
+const qsOpts = {
+  arrayFormat: "repeat"
+}
+
+export const Endpoints = {
+  'Safe Danbooru': "danbooru/safe",
+  Danbooru: "danbooru",
+}
+
+export class BooruApi {
+
+  constructor(endpoint) {
+    this.endpoint = endpoint
+  }
+
+  authenticate(username, password) {
+    const route = `/api/${this.endpoint}/auth`
+
+    const params = {
+      username,
+      password,
+    }
+
+    return fetch(route + '?' + qs.stringify(params, qsOpts))
+      .then(res => res.json())
+      .then(res => res.isSuccess && res.data)
+  }
+
+  getPosts(tags, page, limit) {
+    const route = `/api/${this.endpoint}/posts`
+
+    const params = {
+      page,
+      limit,
+      tags,
+      // tagOrder: BooruApi.TagOrder,
+    }
+
+    return fetch(route + '?' + qs.stringify(params, qsOpts))
+      .then(res => res.json());
+  }
+
+  get imageRoute() {
+    return `/api/${this.endpoint}/image/`
+  }
+
+  getAutocompleteSuggestions(tag) {
+    const route = `/api/${this.endpoint}/autocomplete`
+
+    const params = {
+      tag
+    }
+
+    return fetch(route + '?' + qs.stringify(params, qsOpts))
+      .then(res => res.json())
+  }
+
+  addFavorite(postId) {
+    const route = `/api/${this.endpoint}/favorites/add/${postId}`
+
+    return fetch(route)
+      .then(res => res.json());
+  }
+
+  removeFavorite(postId) {
+    const route = `/api/${this.endpoint}/favorites/remove/${postId}`
+
+    return fetch(route)
+      .then(res => res.json());
+  }
+
+}
