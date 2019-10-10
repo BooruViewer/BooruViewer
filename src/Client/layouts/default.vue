07eb6a8ba293038a7cc4808c77e3178f230a46a2
Nikey646 <nikey646@hotmail.com>
2019-10-10 20:46:55 +1100

    [Client] Added the NavigationPart to the default layout

--- src/Client/layouts/default.vue
+++ src/Client/layouts/default.vue
@@ -1,5 +1,8 @@
 <script>
+  import NavigationPart from "~/components/Parts/NavigationPart"
+
   export default {
+    components: { NavigationPart },
     data() {
       return {
         clipped: false,
@@ -32,21 +35,10 @@
       },
     },
     render: function (h) {
-      const navItems = this.items.map(item => {
-        return <v-list-item to={item.to} router="" exact="">
-          <v-list-item-action>
-            <v-icon>{item.icon}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>{item.title}</v-list-item-content>
-        </v-list-item>
-      })
-
       return <v-app>
-        <v-navigation-drawer v-model={this.drawer} mini-variant={this.miniVariant} clipped={this.clipped} fixed app>
-          <v-list>
-            {navItems}
-          </v-list>
-        </v-navigation-drawer>
+
+        <navigation-part />
+
         <v-app-bar clipped-left fixed="" app>
           <v-app-bar-nav-icon onClick={this.stop(() => this.drawer = !this.drawer)}/>
           <v-btn icon onClick={this.stop(() => this.miniVariant = !this.miniVariant)}>
