9fd050fc63863c29eebf07d1044b83ab32d7e163
Nikey646 <nikey646@hotmail.com>
2020-01-01 19:45:02 +1100

    [Client] Setup nuxt.config.js from previous UI version [Client] Added blank files required for nuxt to run with the new config

--- src/Client/nuxt.config.js
+++ src/Client/nuxt.config.js
@@ -6,16 +6,29 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    title: 'Booru Viewer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Allowing you to easily view your boorus.',
+      },
+      { name: 'msapplication-TileColor', content: '#9f00a7' },
+      { name: 'msapplication-TileImage', content: '/mstile-144x144.png7' },
+      { name: 'theme-color', content: '#FFFFFF' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '180x180', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '180x180', href: '/favicon-194x194.png' },
+      { rel: 'icon', type: 'image/png', sizes: '180x180', href: '/android-chrome-192x192.png' },
+      { rel: 'icon', type: 'image/png', sizes: '180x180', href: '/favicon-16x16.png' },
+      { rel: 'manifest', sizes: '180x180', href: '/site.webmanifest' },
+      { rel: 'mask-icon', color: '#5bbad5', href: '/safari-pinned-tab.svg' },
+    ],
   },
   /*
   ** Customize the progress-bar color
@@ -25,17 +38,21 @@ export default {
   ** Global CSS
   */
   css: [
+    "overlayscrollbars/css/OverlayScrollbars.css",
+    "~/assets/global.scss",
   ],
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
+    "~/plugins/components.js"
   ],
   /*
   ** Nuxt.js dev-modules
   */
   buildModules: [
     '@nuxtjs/vuetify',
+    '@nuxtjs/moment',
   ],
   /*
   ** Nuxt.js modules
@@ -45,12 +62,17 @@ export default {
     '@nuxtjs/axios',
     // Doc: https://github.com/nuxt-community/dotenv-module
     '@nuxtjs/dotenv',
+    'nuxt-vuex-router-sync',
   ],
   /*
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
+    browserBaseURL: "/",
+  },
+  router: {
+    middleware: 'wait-for-store'
   },
   /*
   ** vuetify module configuration
@@ -58,11 +80,14 @@ export default {
   */
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
+    // This breaks production builds because default is false for dev, true for  production
+    // To use this with "true", would require manually importing all vuetify components.
+    treeShake: false,
     theme: {
       dark: true,
       themes: {
         dark: {
-          primary: colors.blue.darken2,
+          primary: colors.purple.darken1,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
@@ -77,10 +102,21 @@ export default {
   ** Build configuration
   */
   build: {
+    extractCSS: process.env.NODE_ENV === 'production',
+    babel: {
+      plugins: [
+        ["@babel/plugin-proposal-decorators", { legacy: true }],
+        ["@babel/plugin-proposal-class-properties", { loose: true }],
+      ]
+    },
     /*
     ** You can extend webpack config here
     */
     extend (config, ctx) {
     }
-  }
+  },
+  generate: {
+    dir: '../Server/wwwroot',
+    // devtools: 'true',
+  },
 }
