2c55aa5eebcafb5172ccee875806d631c5a21665
Nikey646 <nikey646@hotmail.com>
2020-01-05 13:15:18 +1100

    [Client] Added missing mixin for TagsPresenter

---
+++ src/Client/mixins/HumanizeMixin.vue
@@ -0,0 +1,29 @@
+<script>
+    import {Component, Vue} from "nuxt-property-decorator"
+    import flow from "lodash/fp/flow"
+    import map from "lodash/fp/map"
+    import replace from "lodash/fp/replace"
+    import split from "lodash/fp/split"
+    import capitalize from "lodash/fp/capitalize"
+    import join from "lodash/fp/join"
+
+    @Component({})
+    export default class HumanizeMixin extends Vue {
+
+      get HumanizeFunc() {
+        return flow(replace(/_/g, " "),
+          replace(/\(/g, "( "),
+          replace(/\//g, "/ "),
+          split(" "),
+          map(capitalize),
+          join(" "),
+          replace(/\/ /g, "/"),
+          replace(/\( /g, "("))
+      }
+
+    }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/Client/mixins/index.js
@@ -0,0 +1,1 @@
+export { default as HumanizeMixin } from "./HumanizeMixin"
